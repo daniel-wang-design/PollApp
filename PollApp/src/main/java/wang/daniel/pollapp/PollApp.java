2c88fe3ca6083f48c70dd1389cf8283f9217070b
daniel-wang-design <daniel.wang04@hotmail.com>
2022-09-28 22:24:24 -0400

    added support to read user input for slack command

--- PollApp/src/main/java/wang/daniel/pollapp/PollApp.java
+++ PollApp/src/main/java/wang/daniel/pollapp/PollApp.java
@@ -5,15 +5,16 @@ import com.slack.api.bolt.jetty.SlackAppServer;
 
 /**
  *
- * @author wangd
- * project started September 12, 2022
+ * @author wangd project started September 12, 2022
  */
 public class PollApp {
 
     public static void main(String[] args) throws Exception {
         App app = new App();
-        app.command("/hello", (req, ctx) -> {
-            return ctx.ack(":wave: Hello, it's Daniel!");
+        app.command("/test-poll", (req, ctx) -> {
+            String userInput = req.getPayload().getText();
+            ctx.respond("You said: " + userInput); // perform an HTTP request
+            return ctx.ack(); // respond with 200 OK
         });
 
         SlackAppServer server = new SlackAppServer(app);
