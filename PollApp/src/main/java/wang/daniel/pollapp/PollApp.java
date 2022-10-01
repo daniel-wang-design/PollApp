5bfcbfb56b39068f4bd849f3b581aa9e871c4c33
daniel-wang-design <daniel.wang04@hotmail.com>
2022-10-01 18:49:53 -0400

    Added message responses using Webhooks

--- PollApp/src/main/java/wang/daniel/pollapp/PollApp.java
+++ PollApp/src/main/java/wang/daniel/pollapp/PollApp.java
@@ -2,7 +2,9 @@ package wang.daniel.pollapp;
 
 import com.slack.api.bolt.App;
 import com.slack.api.bolt.jetty.SlackAppServer;
+import com.slack.api.webhook.WebhookResponse;
 
+// ngrok http 3000
 /**
  *
  * @author wangd project started September 12, 2022
@@ -13,7 +15,15 @@ public class PollApp {
         App app = new App();
         app.command("/test-poll", (req, ctx) -> {
             String userInput = req.getPayload().getText();
-            ctx.respond("You said: " + userInput); // perform an HTTP request
+            String channelId = req.getPayload().getChannelId();
+            String channelName = req.getPayload().getChannelName();
+
+            System.out.println("Userinput=" + userInput);
+            System.out.println("channelId=" + channelId);
+            System.out.println("channelName=" + channelName);
+            ctx.respond(res -> res
+                    .responseType("in_channel") // respond message visible to everyone in channel
+                    .text("You just said: " + userInput));
             return ctx.ack(); // respond with 200 OK
         });
 
